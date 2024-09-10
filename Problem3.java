3c5da8c76be68db1d7fbab1c60b7fa8973882f1a
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-10 12:12:55 -0700

    finished problem 3

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,13 @@
-
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    int value = 9021; 
+    System.out.println("This is the value: " + value);
+    System.out.println("Here are the digits:");
+    System.out.println(value/10/10/10 % 10);
+    System.out.println(value/10/10 % 10);
+    System.out.println(value/10 % 10);
+    System.out.println(value % 10);
+  }
+}
