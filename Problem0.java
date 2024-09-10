93e923dedec6a8834f5542d9f92b7fd22c16a596
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-10 10:42:54 -0700

    finished problem 0

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,10 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double someVar =3;
+    System.out.println(someVar);
+
+    int other = 3.0;
+    System.out.println(other);
   }
 }
