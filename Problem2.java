a1640888a2fb5add524b356c3559856f7f775116
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-10 11:24:01 -0700

    finished problem 2

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,11 @@
-
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    int value = 497; 
+    System.out.println("Here are the digits:");
+    System.out.println(value % 10);
+    System.out.println(value/10 % 10);
+    System.out.println(value/10/10 % 10);
+  }
+}
