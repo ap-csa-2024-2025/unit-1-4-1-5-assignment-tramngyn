8732c68fcdaf06c9f63ef200342b7198365b2e14
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-10 12:11:45 -0700

    made edits to problem 2

--- Problem2.java
+++ Problem2.java
@@ -3,9 +3,10 @@ public class Problem2
   public static void main(String[] args)
   {
     int value = 497; 
+    System.out.println("This is the value: " + value);
     System.out.println("Here are the digits:");
-    System.out.println(value % 10);
-    System.out.println(value/10 % 10);
     System.out.println(value/10/10 % 10);
+    System.out.println(value/10 % 10);
+    System.out.println(value % 10);
   }
 }
