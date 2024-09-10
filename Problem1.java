622571e481909d50d292d62e0ad37236d4317fcb
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-10 11:19:35 -0700

    finished problem 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,32 @@
+public class Problem1
+{
+  public static void main(String[] args)
+  {
+    int value = 24;
+    System.out.println("Initial value: " + value);
 
+    value++;
+    System.out.println("number is now " + value); // 25 
+
+    value++;
+    System.out.println("number is now " + value); // 26
+
+    value++;
+    System.out.println("number is now " + value); // 27
+
+    value++;
+    System.out.println("number is now " + value); // 28
+
+    value--;
+    System.out.println("number is now " + value); // 27
+
+    value--;
+    System.out.println("number is now " + value); // 26
+
+    value--;
+    System.out.println("number is now " + value); // 25
+
+    value--;
+    System.out.println("number is now " + value); // 24
+  }
+}
